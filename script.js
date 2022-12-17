a492d298f6cc2e1785159f2b12a323ff1a086ac5
Ashwanikumar1715 <ashwanikumar01011997123@gmail.com>
2022-12-17 14:14:59 +0530

    amazing typing animation

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    var typed=new Typed(".typing",{
+        strings: ["learn c", "learn web designing", "learn how to build projects","learn dsa"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop:true
+    });
+});
